a4cb146d5596ef2f6191dea7d4182fb5f26e37b0
Đỗ Đình Duy <3d1911.doduy@gmail.com>
2024-06-30 16:39:43 +0700

    ẩn hiện icon

--- public/client/js/chat.js
+++ public/client/js/chat.js
@@ -1,38 +1,38 @@
 // client_send_mess
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
 // lấy ra form chat
 const formSendData = document.querySelector(".chat .inner-form")
 // console.log(formSendData)
 // check xem form chat có tồn tại hay không 
-if(formSendData){
+if (formSendData) {
     // bắt sự kiện submit của form 
-    formSendData.addEventListener("submit",(e) =>{
+    formSendData.addEventListener("submit", (e) => {
         // dùng  preventDefault() để ngăn sự kiện mặc định của form ở đây là 
         // sự kiện submit của form để lấy ra data
         e.preventDefault();
         // lấy ra value của form đã submt lên 
         const content = e.target.elements.content.value
         // console.log(content)
-        if(content){
-            socket.emit("client_send_mess",content)
+        if (content) {
+            socket.emit("client_send_mess", content)
             e.target.elements.content.value = ""
         }
     })
 }
 //end  client_send_mess
 // sever_return _mess
-socket.on("server_return_mess",(data)=>{
+socket.on("server_return_mess", (data) => {
     const body = document.querySelector(".chat .inner-body");
     const div = document.createElement("div")
     const myId = document.querySelector("[my-id]").getAttribute("my-id")
     let HtmlFullName = ""
-    if(myId == data.userId){
+    if (myId == data.userId) {
         div.classList.add("inner-outgoing")
     }
-    else 
-    {
+    else {
         div.classList.add("inner-incoming")
         HtmlFullName = `<div class="inner-name"> ${data.fullName} </div>`
-        
+
 
     }
     div.innerHTML = (`
@@ -41,13 +41,13 @@ socket.on("server_return_mess",(data)=>{
         `
     )
 
-        body.appendChild(div)
-        bodyChat.scrollTop = bodyChat.scrollHeight;
+    body.appendChild(div)
+    bodyChat.scrollTop = bodyChat.scrollHeight;
 
 })
 // scroll chat 
 const bodyChat = document.querySelector(".chat .inner-body")
-if(bodyChat){
+if (bodyChat) {
     bodyChat.scrollTop = bodyChat.scrollHeight;
 }
 // end scrool chat
@@ -55,10 +55,26 @@ if(bodyChat){
 // document.querySelector('emoji-picker')
 //   .addEventListener('emoji-click', event => console.log(event.detail));
 const emojiPicker = document.querySelector("emoji-picker")
-if(emojiPicker){
+
+// ẩn hiện icon
+const buttonIcon = document.querySelector(".button-icon")
+if (buttonIcon) {
+    const toolTip = document.querySelector(".tooltip");
+    console.log(toolTip)
+    Popper.createPopper(buttonIcon, toolTip)
+
+    buttonIcon.onclick = () => {
+        toolTip.classList.toggle('shown')
+    }
+
+
+}
+//  end ẩn hiện icon
+//  inrsrt icon
+if (emojiPicker) {
     const inputChat = document.querySelector(".chat .inner-form input[name='content']");
     // console.log(inputChat)
-    emojiPicker.addEventListener("emoji-click",(event)=>{
+    emojiPicker.addEventListener("emoji-click", (event) => {
         const icon = event.detail.unicode;
         // console.log(icon)
         inputChat.value = inputChat.value + icon;
